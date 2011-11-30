6033165aee037ee759dd464653dfc44ac3b014b7
ragingwind@gmail.com <ragingwind@gmail.com>
2011-11-30 22:59:45 +0900

    Add icon, profpic images, importing json data, dynamic sidebar item

---
+++ jquerymobile/facebook/facebook-data.js
@@ -0,0 +1,182 @@
+var facebook_newsfeeds = [
+    { 
+		uid:0,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:1,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:2,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:3,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:4,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:5,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:6,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:7,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:8,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:9,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:10,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	},
+	{ 
+		uid:11,
+		actor:{
+			name:"ragingwind",
+			profpic:"images/profpic/ragingwind.jpg"
+		},
+		msg: "jQuery Mobile(1.0, final)로 구현한 facebook 모바일 페이지 예제입니다. 최대한 jQuery Mobile 구조를 따라 제작하였습니다.",
+		attachment:{
+			img:"images/profpic/ragingwind.jpg", 
+			desc:"baby"
+		}, 
+		action: "12 Hours age",
+		like:9,
+		comment:10
+	}
+];
---
+++ jquerymobile/facebook/sidebar-data.js
@@ -0,0 +1,41 @@
+var sidebars = [
+    {
+		label:"Ragingwind",
+		icon:"images/profpic/ragingwind.jpg",
+		href:"#page"
+	},
+	// Divider, divider has a empty href
+	{
+		label:"Favorites"
+	},
+	{
+		label:"News Feed",
+		icon:"images/icon/news-feed.jpg",
+		href:"#page"
+	},
+	{
+		label:"Messages",
+		icon:"images/icon/messages.jpg",
+		href:"#page"
+	},
+	{
+		label:"Nearby",
+		icon:"images/icon/nearby.jpg",
+		href:"#page"
+	},
+	{
+		label:"Friends",
+		icon:"images/icon/friends.jpg",
+		href:"#page"
+	},
+	// Divider, divider has a empty href
+	{
+		label:"List"
+	},
+	{
+		label:"Close Friends",
+		icon:"images/icon/close-friends.png",
+		href:"#page"
+	},
+	
+];
